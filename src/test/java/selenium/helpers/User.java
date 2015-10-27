e8d34520d931734bfac9ce4f7a8217d995a32461
Godzaytsa <eugen.solodkyy@gmail.com>
2015-10-27 09:44:27 +0200

    Changes to User credentials

--- src/test/java/selenium/helpers/User.java
+++ src/test/java/selenium/helpers/User.java
@@ -4,6 +4,6 @@ package selenium.helpers;
  * Created by Gadzilla on 25.10.2015.
  */
 public class User {
-    public static final String email = "gadzilla.test@gmail.com";
-    public static final String password = "gadzilla.test1983";
+    public static final String email = "";
+    public static final String password = "";
 }
